d40633eb8c8bf1d8241cbe5d6960dfb89a95d5f3
RomanenkoBoris <125497888+RomanenkoBoris@users.noreply.github.com>
2023-02-26 11:18:05 +0100

    Basic Java, homework1, level2
    I've create three classes in the separate packages with a certain message.

---
+++ Class1.java
@@ -0,0 +1,9 @@
+package de.telran.package1;
+
+public class Class1 {
+    public static void main (String [] args) {
+
+        System.out.println("Привет. Я - Class1");
+    }
+
+}
---
+++ Class3.java
@@ -0,0 +1,7 @@
+package de.telran.package3;
+
+public class Class3 {
+    public static void main(String[] args){
+        System.out.println("Привет. Я - Class3");
+    }
+}
